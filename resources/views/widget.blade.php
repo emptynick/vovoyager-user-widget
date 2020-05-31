039c751ef686897e67f0e824c31178be89f77c3a
Christoph Schweppe <info@cschweppe.de>
2020-05-31 17:24:11 +0200

    Show some simple data

--- resources/views/widget.blade.php
+++ resources/views/widget.blade.php
@@ -1,1 +1,13 @@
-Here should be some stats about your users 🤷‍♂️
+<div class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-{{ $timestamps ? 3 : 1 }}">
+    <card title="Total">
+        {{ $count }}
+    </card>
+    @if ($timestamps)
+    <card title="This month">
+        {{ $this_month }}
+    </card>
+    <card title="This year">
+        {{ $this_year }}
+    </card>
+    @endif
+</div>
--- src/UserWidget.php
+++ src/UserWidget.php
@@ -2,11 +2,11 @@
 
 namespace Emptynick\UserWidget;
 
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Route;
+use Carbon\Carbon;
 use Illuminate\View\View;
-use Voyager\Admin\Classes\Formfield;
 use Voyager\Admin\Contracts\Plugins\WidgetPlugin;
+use Voyager\Admin\Facades\Voyager as VoyagerFacade;
+use Voyager\Admin\Manager\Plugins as PluginManager;
 
 class UserWidget implements WidgetPlugin
 {
@@ -48,11 +48,34 @@ class UserWidget implements WidgetPlugin
 
     public function getWidgetView(): View
     {
-        return view('userwidget::widget');
+        $auth = VoyagerFacade::auth();
+        $model_name = get_class($auth->user());
+        $model = new $model_name();
+        $count = $model->count();
+        $timestamps = $model->timestamps ?? false;
+
+        $this_month = 0;
+        $this_year = 0;
+        if ($timestamps) {
+            $this_month = $model->whereMonth('created_at', '=', Carbon::now()->subMonth()->month)->count();
+            $this_year = $model->whereYear('created_at', date('Y'))->count();
+        }
+
+        return view('userwidget::widget', compact('count', 'timestamps', 'this_month', 'this_year'));
     }
 
     public function getWidth(): int
     {
-        return 3;
+        return 6;
+    }
+
+    public function getTitle(): ?string
+    {
+        return 'Users';
+    }
+
+    public function getIcon(): ?string
+    {
+        return 'users';
     }
 }
